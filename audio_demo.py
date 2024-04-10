d61f2d1ce3536bd82e10b9400786827e44491676
Anushree Bhuskute <102479736+Anushree-B@users.noreply.github.com>
2024-04-10 14:04:03 +0530

    Update audio_demo.py

--- audio_demo.py
+++ audio_demo.py
@@ -28,18 +28,57 @@ from streamlit_mic_recorder import mic_recorder
 # if __name__ == "__main__":
 #     main()
 
+# import streamlit as st
+# from streamlit_mic_recorder import mic_recorder
+# import speech_recognition as sr
+
+# def main():
+#     st.title('Speech-to-Text with Streamlit')
+
+#     audio = mic_recorder(
+#         start_prompt="Start recording",
+#         stop_prompt="Stop recording",
+#         just_once=True,
+#     )
+
+#     if audio is not None:
+#         # Transcribe audio to text
+#         text = transcribe_audio(audio)
+#         st.write("Recorded Text:", text)
+
+# def transcribe_audio(audio_file):
+#     # Transcribe audio to text
+#     r = sr.Recognizer()
+#     try:
+#         with sr.AudioFile(audio_file) as source:
+#             audio_data = r.record(source)
+    #     text = r.recognize_google(audio_data)
+    #     return text
+    # except sr.UnknownValueError:
+    #     st.write("Unable to recognize speech")
+    #     return ""
+    # except sr.RequestError as e:
+    #     st.write("Error fetching results from Google Speech Recognition service; {0}".format(e))
+#         return ""
+
+# if __name__ == "__main__":
+#     main()
+
 import streamlit as st
 from streamlit_mic_recorder import mic_recorder
 import speech_recognition as sr
+import tempfile
 
 def main():
     st.title('Speech-to-Text with Streamlit')
 
-    audio = mic_recorder(
-        start_prompt="Start recording",
-        stop_prompt="Stop recording",
-        just_once=True,
-    )
+    with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+        audio = mic_recorder(
+            start_prompt="Start recording",
+            stop_prompt="Stop recording",
+            just_once=True,
+            output_filename=temp_audio_file.name
+        )
 
     if audio is not None:
         # Transcribe audio to text
