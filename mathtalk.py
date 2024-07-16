e5698dc853c7fa7f8270cd140b9986afd58e139e
Anushree Bhuskute <102479736+Anushree-B@users.noreply.github.com>
2024-07-16 09:29:46 +0530

    Update mathtalk.py

--- mathtalk.py
+++ mathtalk.py
@@ -146,13 +146,13 @@ def from_microphone():
     audio = audiorecorder("Click to record", "Click to stop recording")
     if len(audio)>0:
         st.write("audio")
-        st.audio(audio.export().read()) 
+        st.audio(audio.export().read(), format ='audio/wav') 
         st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")  
         r = sr.Recognizer()
         st.write("Recognizer called")
         audio_file_path = "temp_audio_file.wav"
         with open(audio_file_path, "wb") as f:
-            f.write(audio)
+            f.write(audio.export().read())
         with sr.AudioFile(audio_file_path) as source:
             audio_text = r.listen(source)
             st.write("audio text recorded")
