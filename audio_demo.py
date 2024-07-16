5ec3e24b411ee7e77dd8dd3e9e4e1f22bc69a98d
Anushree Bhuskute <102479736+Anushree-B@users.noreply.github.com>
2024-07-16 11:38:06 +0530

    Update audio_demo.py

--- audio_demo.py
+++ audio_demo.py
@@ -7,7 +7,7 @@ audio = audiorecorder("Click to record", "Click to stop recording")
 
 if len(audio) > 0:
     # To play audio in frontend:
-    st.audio(audio.export().read())  
+    st.audio(audio.export().read(), format ='audio/wav')  
 
     # To get audio properties, use pydub AudioSegment properties:
     st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
